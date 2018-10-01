29f6ed1166d0638d032a3223beec46b16ab6dd3c
Gilbert Torchon <gilbert@noukod.com>
2018-10-01 14:42:39 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,7 @@
 class Book
+
+  GENRES=["Thriller","Science Fiction","Romance"]
+
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
@@ -9,5 +12,6 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
 
 end
